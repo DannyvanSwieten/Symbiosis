5fb9fc1151b32aa27ea0086e21904a2c7dda5405
Danny <danny@resolume.com>
2018-02-19 16:16:43 +0100

    Initial Commit

---
+++ ecs3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ecs3
+//
+//  Created by Danny on 19/02/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
